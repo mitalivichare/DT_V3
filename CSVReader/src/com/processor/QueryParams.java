7e87d4d1e33b2a1f58c1dc608bb00cbd3b34caf3
mitalivichare <zaid3891@gmail.com>
2017-07-13 08:39:46 +0530

    Parsing uploaded till extracting parameters of a single where condition

---
+++ CSVReader/src/com/processor/QueryParams.java
@@ -0,0 +1,32 @@
+package com.processor;
+
+public class QueryParams 
+{
+	private String columnlist,criteria,filepath;
+
+	public String getColumnlist() {
+		return columnlist;
+	}
+
+	public void setColumnlist(String columnlist) {
+		this.columnlist = columnlist;
+	}
+
+	public String getCriteria() {
+		return criteria;
+	}
+
+	public void setCriteria(String criteria) {
+		this.criteria = criteria;
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+	
+
+}
